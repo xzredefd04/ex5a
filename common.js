bc2fafe734cf962528db6591339a3d223b71b7f3
Benyada <xzredef.007@gmail.com>
2025-02-05 10:58:22 +0700

    refactor#2 adding paceCal fn.

--- common.js
+++ common.js
@@ -8,6 +8,15 @@ function validate(pace){
 		return {"status":0,"message":msg,"hour":0,"min":0};
 	}
 }
+function paceCal(pace,dist){
+	var min = dist * pace;
+		var hour = 0;
+		if(min > 60){
+			hour = Math.floor(min / 60);          
+			min = min % 60;
+		}
+		return {"min":min,"hour":hour};
+}
 function calculate(p,d){
 		var msg = "";
 		var pace = parseFloat(p);
@@ -16,12 +25,7 @@ function calculate(p,d){
 		var chk = validate(pace);
 		if(chk != null) return chk;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
